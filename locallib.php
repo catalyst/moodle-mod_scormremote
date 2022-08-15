31931bb7a5a003e5a400273717c0f4cc83e815d4
ScottVerbeek <scottverbeek@catalyst-au.net>
2022-08-15 14:25:50 +1000

    Add scorm-again to uploaded scorm package

--- locallib.php
+++ locallib.php
@@ -26,7 +26,8 @@
  /**
  * Extracts scrom package, sets up all variables.
  * Called whenever scorm changes
- * @param object $scorm instance - fields are updated and changes saved into database
+ *
+ * @param object $scormremote instance - fields are updated and changes saved into database
  * @param bool $full force full update if true
  * @return void
  */
@@ -37,7 +38,11 @@ function scormremote_parse($scormremote, $full) {
         $cm = get_coursemodule_from_instance('scormremote', $scormremote->id);
         $scormremote->cmid = $cm->id;
     }
+
     $context = context_module::instance($scormremote->cmid);
+    if (!isset($scormremote->sha1hash)) {
+        $scormremote->sha1hash = null;
+    }
     $newhash = $scormremote->sha1hash;
 
     $fs = get_file_storage();
@@ -52,30 +57,76 @@ function scormremote_parse($scormremote, $full) {
         if (strtolower($packagefile->get_filename()) == 'imsmanifest.xml') {
             $packagefileimsmanifest = true;
         }
-    } else {
-        $newhash = null;
     }
 
-
     if ($packagefile) {
-        if (!$full and $packagefile and $scormremote->sha1hash === $newhash) {
-            if ($packagefileimsmanifest || $fs->get_file($context->id, 'mod_scorm', 'content', 0, '/', 'imsmanifest.xml')) {
-                // No need to update.
-                return;
-            }
-        }
         if (!$packagefileimsmanifest) {
-            // Now extract files.
+            // Delete old files.
             $fs->delete_area_files($context->id, 'mod_scormremote', 'content');
 
+            // Now extract files.
             $packer = get_file_packer('application/zip');
             $packagefile->extract_to_storage($packer, $context->id, 'mod_scormremote', 'content', 0, '/');
-        }
 
-    } else if (!$full) {
-        return;
+            $fileinfo = [
+                'component' => 'mod_scormremote',
+                'filearea'  => 'content',
+                'itemid'    => 0,
+                'contextid' => $context->id,
+                'filepath'  => '/',
+            ];
+
+            // Add javascript to all .html at the root level.
+            $filerecords = $DB->get_records('files', $fileinfo, 'filename');
+            foreach($filerecords as $filerecord) {
+                if (pathinfo($filerecord->filename, PATHINFO_EXTENSION) != 'html') {
+                    continue;
+                }
+
+                $fileinfo['filename'] = $filerecord->filename;
+
+                $file = $fs->get_file($fileinfo['contextid'], $fileinfo['component'], $fileinfo['filearea'],
+                      $fileinfo['itemid'], $fileinfo['filepath'], $fileinfo['filename']);
+
+                $newfilecontents = add_scormagain_html($file->get_content());
+                $file->delete();
+                $fs->create_file_from_string($fileinfo, $newfilecontents);
+            }
+        }
     }
 
     $scormremote->sha1hash = $newhash;
     $DB->update_record('scormremote', $scormremote);
+}
+
+
+/**
+ * Adds required javascript to a HTML string.
+ *
+ * @param string $html
+ * @return mixed Returns the content as a string or returns false when it can't append properly.
+ */
+function add_scormagain_html($html) {
+    global $CFG;
+
+    // Find the closing body tag.
+    $pos = strpos($html, '</body>');
+
+    // No closing body tag.
+    if ($pos === false) {
+        return false;
+    }
+
+    // Is there more then one body tag?
+    if (strpos($html, '</body>', $pos + 1) !== false) {
+        return false;
+    }
+
+    // Create external scormagain javascript element.
+    $external = \html_writer::script('', $CFG->wwwroot.'/mod/scormremote/scorm-again/dist/scorm12.min.js');
+
+    // Create javascript tag containing the API calls.
+    $script = \html_writer::script("var settings = {};window.API = new Scorm12API(settings);window.parent = window;");
+
+    return substr_replace($html, $external.$script, $pos, 0);
 }
