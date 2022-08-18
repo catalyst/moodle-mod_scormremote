0b73a7cb1b13dfe87707198d755792bbf41fc2e9
ScottVerbeek <scottverbeek@catalyst-au.net>
2022-08-18 11:48:55 +1000

    [#2] Bugfix for scorm package validate

--- mod_form.php
+++ mod_form.php
@@ -26,6 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/course/moodleform_mod.php');
+require_once($CFG->dirroot.'/mod/scorm/lib.php');
 
 /**
  * Module instance settings form.
