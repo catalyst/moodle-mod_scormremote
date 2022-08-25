c6ab1d8b263127f10c64c07cd13f074f0010f22d
ScottVerbeek <scottverbeek@catalyst-au.net>
2022-08-25 16:33:01 +1000

    Initial commit layers two and three

---
+++ amd/src/layer2.js
@@ -0,0 +1,487 @@
+const debug = true;
+const output = window.console;
+
+// Define exception/error codes
+var _NoError = { "code": "0", "string": "No Error", "diagnostic": "No Error" };
+var _GeneralException = { "code": "101", "string": "General Exception", "diagnostic": "General Exception" };
+
+var initialized = false;
+
+// local variable definitions
+var apiHandle = null;
+var embeddedWindow = null;
+var CMI = null;
+
+// TODO: Move constants to their own file.
+const EMBEDDED_WINDOW_ID = 'embedded-third-layer';
+const ORIGIN = "https://eca.localhost";
+
+
+function init() {
+    initMessageReciever();
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute("id", EMBEDDED_WINDOW_ID);
+    iframe.setAttribute("src", document.body.dataset.source);
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("height", "100%");
+    iframe.setAttribute("width", "100%");
+    document.body.insertBefore(iframe, document.getElementById("wrapper"));
+    embeddedWindow = iframe.contentWindow;
+    LMSGetDataModel();
+}
+
+/**
+ * Initialize communication with LMS by calling the LMSInitialize function which will be implemented by the LMS.
+ *
+ * @returns {string} true|false depending wheter successful.
+ */
+function LMSInitialize() {
+    if (initialized) {
+        message("LMSInitialize succeeded, already initialized.");
+        return "true";
+    }
+
+    var api = getAPIHandle();
+    if (api == null) {
+        message("Unable to locate the LMS's API Implementation.\nLMSInitialize was not successful.");
+        return "false";
+    }
+
+    var result = api.LMSInitialize("");
+    if (result.toString() != "true") {
+        var err = ErrorHandler();
+        message("LMSInitialize failed with error code: " + err.code);
+    }
+    else {
+        initialized = true;
+        message("LMSInitialized succeeded.")
+    }
+
+    return result.toString();
+}
+
+/**
+ * Close communication with LMS by calling the LMSFinish function which will be implemented by the LMS.
+ *
+ * @returns {string} true|false depending wheter successful.
+ */
+function LMSFinish() {
+    if (!initialized) return "true";
+
+    var api = getAPIHandle();
+    if (api == null) {
+        message("Unable to locate the LMS's API Implementation.\nLMSFinish was not successful.");
+        return "false";
+    }
+    else {
+        // call the LMSFinish function that should be implemented by the API
+        var result = api.LMSFinish("");
+        if (result.toString() != "true") {
+            var err = ErrorHandler();
+            message("LMSFinish failed with error code: " + err.code);
+        }
+    }
+
+    initialized = false;
+
+    return result.toString();
+}
+
+/**
+ * Wraps the call to the LMS LMSGetValue function.
+ *
+ * @param {string} name string representing the cmi data model defined category or element (e.g. cmi_core.student_name).
+ * @returns {string}
+ */
+function LMSGetValue(name) {
+    var api = getAPIHandle();
+    var result = "";
+    if (api == null) {
+        message("Unable to locate the LMS's API Implementation.\nLMSGetValue was not successful.");
+    }
+    else if (!initialized && !LMSInitialize()) {
+        var err = ErrorHandler(); // get why LMSInitialize() returned false
+        message("LMSGetValue failed - Could not initialize communication with the LMS - error code: " + err.code);
+    }
+    else {
+        result = api.LMSGetValue(name);
+
+        var error = ErrorHandler();
+        if (error.code != _NoError.code) {
+            // an error was encountered so display the error description
+            message("LMSGetValue(" + name + ") failed. \n" + error.code + ": " + error.string);
+            result = "";
+        }
+    }
+    return result.toString();
+}
+
+/**
+ * Wraps the call to the LMS LMSGetValue function.
+ *
+ * @param {string} name string representing the cmi data model defined category or element (e.g. cmi_core.student_name).
+ * @param {mixed} value the value that the named element or category will be assigned.
+ * @returns {string} true|false depending wheter successful.
+ */
+function LMSSetValue(name, value) {
+    var api = getAPIHandle();
+    var result = "false";
+    if (api == null) {
+        message("Unable to locate the LMS's API Implementation.\nLMSSetValue was not successful.");
+    }
+    else if (!initialized && !LMSInitialize()) {
+        var err = ErrorHandler(); // get why LMSInitialize() returned false
+        message("LMSSetValue failed - Could not initialize communication with the LMS - error code: " + err.code);
+    }
+    else {
+        result = api.LMSSetValue(name, value);
+        if (result.toString() != "true") {
+            var err = ErrorHandler();
+            message("LMSSetValue(" + name + ", " + value + ") failed. \n" + err.code + ": " + err.string);
+        }
+    }
+
+    return result.toString();
+}
+
+/**
+ * Commits the data to the LMS.
+ *
+ * @returns {string} true|false depending wheter successful.
+ */
+function LMSCommit() {
+    var api = getAPIHandle();
+    var result = "false";
+    if (api == null) {
+        message("Unable to locate the LMS's API Implementation.\nLMSCommit was not successful.");
+    }
+    else if (!initialized && !LMSInitialize()) {
+        var err = ErrorHandler(); // get why LMSInitialize() returned false
+        message("LMSCommit failed - Could not initialize communication with the LMS - error code: " + err.code);
+    }
+    else {
+        result = api.LMSCommit("");
+        if (result != "true") {
+            var err = ErrorHandler();
+            message("LMSCommit failed - error code: " + err.code);
+        }
+    }
+
+    return result.toString();
+}
+
+/**
+ * Call the LMSGetLastError function.
+ *
+ * @returns {string} The error code that was set by the LMS function call.
+ */
+function LMSGetLastError() {
+    var api = getAPIHandle();
+    if (api == null) {
+        message("Unable to locate the LMS's API Implementation.\nLMSGetLastError was not successful.");
+        //since we can't get the error code from the LMS, return a general error
+        return _GeneralException.code; //General Exception
+    }
+
+    return api.LMSGetLastError().toString();
+}
+
+/**
+ * Call the LMSGetErrorString function
+ *
+ * @param {number|string|null} errorCode
+ * @returns {string} The textual description that corresponds to the input error code
+ */
+function LMSGetErrorString(errorCode) {
+    var api = getAPIHandle();
+    if (api == null) {
+        message("Unable to locate the LMS's API Implementation.\nLMSGetErrorString was not successful.");
+        return _GeneralException.string;
+    }
+
+    return api.LMSGetErrorString(errorCode).toString();
+}
+
+/**
+ * Call the LMSGetDiagnostic function
+ *
+ * @param {number|string|null} errorCode
+ * @returns {string} The vendor specific textual description that corresponds to the input error code.
+ */
+function LMSGetDiagnostic(errorCode) {
+    var api = getAPIHandle();
+    if (api == null) {
+        message("Unable to locate the LMS's API Implementation.\nLMSGetDiagnostic was not successful.");
+        return "Unable to locate the LMS's API Implementation. LMSGetDiagnostic was not successful.";
+    }
+
+    return api.LMSGetDiagnostic(errorCode).toString();
+}
+
+/**
+ * Determines if an error was encountered by the previous API call and if so, returns the error.
+ *
+ * Usage:
+ * var last_error = ErrorHandler();
+ * if (last_error.code != _NoError.code)
+ * {
+ *     message("Encountered an error. Code: " + last_error.code +
+ *                                          "\nMessage: " + last_error.string +
+ *                                          "\nDiagnostics: " + last_error.diagnostic);
+ * }
+ *
+ * @returns {object}
+ */
+function ErrorHandler() {
+    var error = { "code": _NoError.code, "string": _NoError.string, "diagnostic": _NoError.diagnostic };
+    var api = getAPIHandle();
+    if (api == null) {
+        message("Unable to locate the LMS's API Implementation.\nCannot determine LMS error code.");
+        error.code = _GeneralException.code;
+        error.string = _GeneralException.string;
+        error.diagnostic = "Unable to locate the LMS's API Implementation. Cannot determine LMS error code.";
+        return error;
+    }
+
+    // check for errors caused by or from the LMS
+    error.code = api.LMSGetLastError().toString();
+    if (error.code != _NoError.code) {
+        // an error was encountered so display the error description
+        error.string = api.LMSGetErrorString(error.code);
+        error.diagnostic = api.LMSGetDiagnostic("");
+    }
+
+    return error;
+}
+
+/**
+ * Returns the handle to API object if it was previously set, otherwise it returns null
+ *
+ * @returns {*}
+ */
+function getAPIHandle() {
+    if (apiHandle == null) {
+        apiHandle = getAPI();
+    }
+
+    return apiHandle;
+}
+
+/**
+ * This function looks for an object names API in parent and opener window.
+ *
+ * @param {Window} win
+ * @returns {*}
+ */
+function findAPI(win) {
+    var findAPITries = 0;
+    while ((win.API == null) && (win.parent != null) && (win.parent != win)) {
+        findAPITries++;
+        // Note: 7 is an arbitrary number, but should be more than sufficient
+        if (findAPITries > 7) {
+            message("Error finding API -- too deeply nested.");
+            return null;
+        }
+
+        win = win.parent;
+    }
+    return win.API;
+}
+
+/**
+ * This function looks for an object named API, first in the current window's frame hierarchy and then, if necessary, in the current
+ * window's opener window hierarchy (if there is an opener window).
+ *
+ * @returns
+ */
+function getAPI() {
+    var theAPI = findAPI(window);
+    if ((theAPI == null) && (window.opener != null) && (typeof (window.opener) != "undefined")) {
+        theAPI = findAPI(window.opener);
+    }
+    if (theAPI == null) {
+        message("Unable to find an API adapter");
+    }
+    return theAPI
+}
+
+// TODO: Move this to UTILS.
+/**
+ * This function outputs messages to a specified output. You can define your own output object. It will just need to implement a
+ * log(string) function. This interface was used so that the output could be assigned the window.console object.
+ *
+ * Depends on:
+ *  - {boolean} debug to indicate if output is wanted
+ *  - {object} output to handle the messages. This object must implement a function log(string).
+ *
+ * @param {string} str
+ * @returns {null}
+ */
+function message(str) {
+    if (debug) {
+        output.log("[LAYER 2]: " + str);
+    }
+}
+
+/**
+ * This function creates the event handles for incoming postMessage. This method depends on the ORIGIN variable, if not set the
+ * function will immediatly return as a security measure. The function expects that the data passed is a object which contains two
+ * properties:
+ *  - {string} function: The method that should be called.
+ *  - {object Array} arguments: The arguments that should be passed.
+ *
+ * Depends on:
+ *  - {string} ORIGIN to check if event origin is coming from expected path
+ *
+ * @returns {null}
+ */
+function initMessageReciever() {
+    window.addEventListener('message', (e) => {
+        // Don't run anything if message is not coming from expected host.
+        if (e.origin !== ORIGIN) {
+            message('Recieved message from unknown origin "' + e.origin + '", (expected: "' + ORIGIN + '")');
+            return;
+        }
+
+        const functionName = e.data['function'];
+        const functionArgs = e.data['arguments'];
+
+        // Can't run unknown function.
+        if (!functionName || typeof window[functionName] !== 'function') {
+            message('Recieved message contains unexpected data for param function, recieved "' + functionName + '"');
+            return;
+        }
+
+        // Can't run function with no arguments passed.
+        // Even when the desired function has no argument, the passed param MUST be a empty array.
+        if (!functionArgs || typeof functionArgs !== 'object' || !Array.isArray(functionArgs)) {
+            message('Recieved message contains unexpected data for param arguments, expected array (recieved "' +
+                Object.prototype.toString.call(functionArgs) + '")');
+            return;
+        }
+
+        message('Message recieved. Calling function: "' + functionName + '"');
+        window[functionName].apply(null, functionArgs);
+    });
+}
+
+/**
+ * This function looks for an iframe element by id EMBEDDED_WINDOW_ID and sets the window object to embeddedWindow. It returns the
+ * value that it finds or null if it fails to find it.
+ *
+ * Depends on:
+ *  - {string} EMBEDDED_WINDOW_ID the id to look for
+ *  - {Window|null} embeddedWindow the variable to set to.
+ *
+ * @returns {Window|null} value contained by embeddedWindow
+ */
+ function getEmbeddedWindow() {
+    if (embeddedWindow === null) {
+        const element = document.findElementById(EMBEDDED_WINDOW_ID);
+
+        if (element === null) {
+            message('Could not find embedded window object, searched for element by id: "'+ EMBEDDED_WINDOW_ID +'"');
+            return null;
+        }
+
+        if (element.tagName !== 'IFRAME') {
+            message('Unexpected tagName for embedded window object, expected "IFRAME" got: "'+ element.tagName +'"');
+            return null;
+        }
+
+        embeddedWindow = element.contentWindow;
+    }
+
+    return embeddedWindow;
+}
+
+/**
+ * This function returns the data model in object form. It will also store it to the global CMI variable.
+ *
+ * @returns {object}
+ */
+function LMSGetDataModel() {
+    if ( CMI == null ) {
+        const result = LMSGetValue('cmi._children');
+        CMI = LMSGetChildren('cmi', result.split(','));
+    }
+    return CMI;
+}
+
+/**
+ * This function returns the children of a data model defined category or element inside a object.
+ *
+ * @param {str} parent
+ * @param {str[]} children
+ * @returns {object}
+ */
+function LMSGetChildren(parent, children) {
+    let child = {};
+    const count = LMSGetNumberStored(parent);
+    if (count !== false) {
+        // Multiple childern.
+
+        for (let i = 0; i < count; i++) {
+            const key = parent + '.' + i;
+            child[i] = LMSGetChildren(key, children)
+        }
+    } else {
+        // Singular children only.
+        for (let i = 0; i < children.length; i++) {
+            const key = parent + '.' + children[i];
+            const supChildren = LMSGetSupportedChildren(key);
+
+            if (supChildren !== false) {
+                child[children[i]] = LMSGetChildren(key, supChildren);
+                continue;
+            }
+
+            child[children[i]] = LMSGetValue(key);
+        }
+    }
+
+    return child;
+}
+
+/**
+ * This function returns the supported data model elements of the a given data model defined category or element. If the given name
+ * does not have any supported children, then a boolean of false will be returned.
+ *
+ * @param {str} name data model defined category or element
+ * @returns {str[]|bool}
+ */
+function LMSGetSupportedChildren(name) {
+    const result = LMSGetValue(name + '._children');
+    if (result === "") {
+        return false;
+    }
+    return result.split(',');
+}
+
+/**
+ * This function return the number of stored elements to this data model element or category. If the element does not support
+ * multiple stored elements then it will return false.
+ *
+ * @param {str} name data model defined category or element
+ * @returns {number|bool}
+ */
+function LMSGetNumberStored(name) {
+    const result = LMSGetValue(name + "._count");
+    if (result === "") {
+        return false;
+    }
+    return parseInt(result);
+}
+
+/**
+ * This function send the CMI aka DataModel to the third layer.
+ *
+ * @returns {null}
+ */
+function postLMSDataModel() {
+    const datamodel = LMSGetDataModel();
+    embeddedWindow.postMessage(
+        { function: 'LMSSetDataModel', arguments: [datamodel] },
+        ORIGIN
+    );
+}
---
+++ amd/src/layer3.js
@@ -0,0 +1,147 @@
+const settings = {
+    // Determines whether the API schedules an autocommit to the LMS after setting a value. |
+    autocommit:            false,
+    // Number of seconds to wait before autocommiting. Timer is restarted if another value is set. |
+    autocommitSeconds:     60,
+    logLevel:              2, // 1 => DEBUG,
+                              // 2 => INFO,
+                              // 3 => WARN,
+                              // 4 => ERROR, // Default.
+                              // 5 => NONE
+    // Function to transform the commit object before sending it to lmsCommitUrl.
+    //requestHandler:        () => {},
+    // Function to be called whenever a message is logged.
+    //onLogMessage:          () => {},
+};
+
+var initialized = false;
+const debug = settings.logLevel <= 2;
+const output = window.console;
+
+// TODO: Move constants to their own file.
+const EMBEDDED_WINDOW_ID = 'embedded-fourth-layer';
+const ORIGIN = "https://scormdiscovery.localhost";
+
+
+/**
+ * Initialize communication with
+ */
+function init() {
+    // Create event listener.
+    initMessageReciever();
+
+    // Setup the API.
+    window.API = new Scorm12API(settings);
+    window.API.on("LMSCommit", postMessageToParent('LMSCommit'));
+    window.API.on("LMSFinish", postMessageToParent('LMSFinish'));
+    window.API.on("LMSSetValue", onLMSSetValue);
+
+    // Ask for the data model to be sent.
+    postMessageToParent('postLMSDataModel', []);
+
+    // TODO: Set took to long to connect timeout.
+}
+
+/**
+ * This function creates the event handler for incoming postMessage. We expect the parent window (which is on a different domain) to
+ * send a single message. This message contains the data model object. We keep listening for errors.
+ * properties:
+ *  - {string} function: The method that should be called [ErrorHandler, LMSSetDataModel].
+ *  - {object Array} arguments: The arguments that should be passed.
+ *
+ * Depends on:
+ *  - {string} ORIGIN to check if event origin is coming from expected path
+ *
+ * @returns {null}
+ */
+ function initMessageReciever() {
+    window.addEventListener('message', (e) => {
+        const ALLOWED_METHODS = ['ErrorHandler', 'LMSSetDataModel', 'message'];
+
+        // Don't run anything if message is not coming from expected host.
+        if (e.origin !== ORIGIN) {
+            message('Recieved message from unknown origin "' + e.origin + '", (expected: "' + ORIGIN + '")');
+            return;
+        }
+
+        const functionName = e.data['function'];
+        const functionArgs = e.data['arguments'];
+
+        // Can't run unknown function.
+        if (
+            !functionName ||
+            !ALLOWED_METHODS.includes(functionName) ||
+            typeof window[functionName] !== 'function'
+        ) {
+            message('Recieved message contains unexpected data for param function, recieved "' + functionName + '"');
+            return;
+        }
+
+        // Can't run function with no arguments passed.
+        // Even when the desired function has no argument, the passed param MUST be a empty array.
+        if (!functionArgs || typeof functionArgs !== 'object' || !Array.isArray(functionArgs)) {
+            message('Recieved message contains unexpected data for param arguments, expected array (recieved "' +
+                Object.prototype.toString.call(functionArgs) + '")');
+            return;
+        }
+
+        message('Message recieved. Calling function: "' + functionName + '"');
+        window[functionName].apply(null, functionArgs);
+    });
+}
+
+/**
+ * This function outputs messages to a specified output. You can define your own output object. It will just need to implement a
+ * log(string) function. This interface was used so that the output could be assigned the window.console object.
+ *
+ * Depends on:
+ *  - {boolean} debug to indicate if output is wanted
+ *  - {object} output to handle the messages. This object must implement a function log(string).
+ *
+ * @param {string} str
+ * @returns {null}
+ */
+ function message(str) {
+    if (debug) {
+        output.log("[LAYER 3]: " + str);
+    }
+}
+
+/**
+ * This function allows for the data model to be set prior to LMSInitialize. After setting the data model we call loadContent().
+ * This must be run before SCO content in loaded.
+ *
+ * @param {object} cmi
+ */
+function LMSSetDataModel(cmi) {
+    window.API.loadFromJSON(cmi);
+
+    loadContent();
+}
+
+/**
+ * Load the requested SCO content.
+ *
+ */
+ function loadContent() {
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute("id", EMBEDDED_WINDOW_ID);
+    iframe.setAttribute("src", document.body.dataset.source);
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("height", "100%");
+    iframe.setAttribute("width", "100%");
+    document.body.insertBefore(iframe, document.getElementById("wrapper"));
+}
+
+function onLMSSetValue(name, value) {
+    message('Setting "' + name + '" to value: "' + value + '"');
+    postMessageToParent('LMSSetValue', [name, value]);
+};
+
+
+function postMessageToParent(functionName, arguments = []) {
+    window.parent.postMessage(
+        {function: functionName, arguments},
+        ORIGIN
+    );
+}
