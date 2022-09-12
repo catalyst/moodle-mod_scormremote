73ac595a710d048ba57cb01c4f8a1b95008b0404
ScottVerbeek <scottverbeek@catalyst-au.net>
2022-09-12 16:41:14 +1000

    Make the enrol role a setting

--- lib.php
+++ lib.php
@@ -226,9 +226,9 @@ function scormremote_pluginfile($course, $cm, $context, $filearea, $args, $force
         // Check if this user is_enrolled in this course.
         if (!is_enrolled($context, $user)) {
             $instance = $DB->get_record('enrol', ['courseid' => $course->id, 'enrol' => 'manual']);
-            $student = reset(get_archetype_roles('student'));
+            $roleid = get_config('mod_scormremote', 'roleid') ?? null;
             $enrolplugin = enrol_get_plugin($instance->enrol);
-            $enrolplugin->enrol_user($instance, $user->id, $student->id ?? null);
+            $enrolplugin->enrol_user($instance, $user->id, $roleid);
         }
 
         $revision = (int)array_shift($args); // Prevents caching problems - ignored here.
